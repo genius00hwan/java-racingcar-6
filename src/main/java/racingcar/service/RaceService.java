379d00cccb48163b4006756307671ac38df94f9c
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-12-13 00:53:48 +0900

    rename(RaceService) : rename to RaceService from AdvanceService

--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -7,32 +7,47 @@ import static racingcar.config.Constants.REFERENCE_OF_ADVANCE;
 import static racingcar.config.Constants.STAY_VALUE;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.List;
-import racingcar.model.Car;
 
-public class AdvanceService {
+import java.util.ArrayList;
+import java.util.List;
+
+import racingcar.model.Car;
+import racingcar.model.Round;
+
+public class RaceService {
     private static class SingletonAdvanceService {
-        private static final AdvanceService ADVANCE_SERVICE = new AdvanceService();
+        private static final RaceService ADVANCE_SERVICE = new RaceService();
     }
 
-    public static AdvanceService getInstance() {
+    public static RaceService getInstance() {
         return SingletonAdvanceService.ADVANCE_SERVICE;
     }
 
-    private AdvanceService() {
+    private RaceService() {
     }
 
-    public void advanceCars(List<Car> cars) {
+    public List<Car> race(Round round, List<Car> cars) {
+        List<Car> completedCars = new ArrayList<>();
+        while (round.hasMoreRound()) {
+            completedCars = advanceCars(cars);
+            round.completeARound();
+        }
+        return completedCars;
+    }
+
+    private List<Car> advanceCars(List<Car> cars) {
         for (Car car : cars) {
             int movingDistance = generateMovingDistance();
             car.move(movingDistance);
         }
+
+        return cars;
     }
 
     private Integer generateMovingDistance() {
         int randomNumber = Randoms.pickNumberInRange(MINIMUM_OF_RANDOM_NUMBER.get(),
                 MAXIMUM_OF_RANDOM_NUMBER.get());
-        if (randomNumber > REFERENCE_OF_ADVANCE.get()) {
+        if (randomNumber >= REFERENCE_OF_ADVANCE.get()) {
             return ADVANCE_DISTANCE.get();
         }
         return STAY_VALUE.get();
