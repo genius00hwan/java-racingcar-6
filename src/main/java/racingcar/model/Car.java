be5ec1ae2aa3514a7abcf3dde915693f6d4cc5f9
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-12-12 22:02:31 +0900

    feat(Car) : create Car class

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,37 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car {
+    private String name;
+    private List<Integer> locationHistory;
+    private Boolean winningOrNot;
+
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+        locationHistory = new ArrayList<>();
+        locationHistory.add(0);
+        winningOrNot = false;
+    }
+
+    public Integer getCurrentLocation() {
+        return locationHistory.get(locationHistory.size() - 1);
+    }
+
+    public void move(Integer movingDistance) {
+        Integer nextLocation = getCurrentLocation() + movingDistance;
+        locationHistory.add(nextLocation);
+    }
+
+    public Integer getLocationOfRound(Integer round){
+        return locationHistory.get(round);
+    }
+    private void validateName(String name) {
+        if (name.length() >= 5) {
+            throw new IllegalArgumentException("잘못된 이름입니다.");
+        }
+    }
+
+}
